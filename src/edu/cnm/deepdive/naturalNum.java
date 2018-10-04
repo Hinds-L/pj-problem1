7cd9727aad8e8ca9489df690fea19b5547e470e0
Leslie <leslie.hinds82@gmail.com>
2018-10-04 01:12:55 -0600

    Initial commit

---
+++ src/edu/cnm/deepdive/naturalNum.java
@@ -0,0 +1,20 @@
+package edu.cnm.deepdive;
+
+public class naturalNum {
+  int sum;
+  int i;
+
+  public static void main(String[] args) {
+
+    private static int get.Sum(){
+    if (int = 1; i <= 1000; i++){
+          (i % 3 == 0 || i % 5 == 0);
+
+    sum += 1;
+
+
+  }
+ return sum;
+    }
+  }
+}
